0f9797e4b57d2e9a089ae106b1561102f39f1ea1
Jose Luis <joseluis.pueyo@estudiants.urv.cat>
2025-06-09 00:28:21 +0200

    Problem1.2: Started, first example character segmentation working

--- problem1.m
+++ problem1.m
@@ -1,8 +1,7 @@
+ex1(2, true)
 
-ex1(1, "other")
-
-function []=ex1(difficulty, mode)
-    filelist = loadFiles(difficulty, mode);
+function []=ex1(difficulty, live)
+    filelist = loadFiles(difficulty, live);
     templates = loadTemplates(difficulty);
 
     for i = 1:length(filelist)
@@ -12,7 +11,7 @@ function []=ex1(difficulty, mode)
         %figure('Name', 'Imagen original'), imshow(im);
         
         %% 2. Detección de la region de la matricula
-        imPlate = detectPlateRegionCE(im);
+        imPlate = detectPlateRegion(im, difficulty);
         %figure('Name', 'Placa recortada'), imshow(imPlate);
         
         %% 3. Extraer caracteres
@@ -28,23 +27,30 @@ function []=ex1(difficulty, mode)
     
 end
 
-function filelist = loadFiles(difficulty, mode)
+function filelist = loadFiles(difficulty, live)
     if(difficulty == 1)
         % Entorno controlado
-        if(strcmp(mode, "live"))
-            cam1 = ipcam('http://10.112.11.211/video.mjpg', 'root', 'Vivotek1', 'Timeout', 10);
-            cam2 = ipcam('http://10.112.11.212/video.mjpg', 'root', 'Vivotek2', 'Timeout', 10);
-            cam3 = ipcam('http://10.112.11.213/video.mjpg', 'root', 'Vivotek3', 'Timeout', 10);
-            
-            filelist = [ snapshot(cam1) snapshot(cam2) snapshot(cam3)];
+        if(live)
+            filelist = getLiveImages();
         else
-            filelist = dir(fullfile('images/problem1.1', '**\*.*'));
+            filelist = dir(fullfile('images/problem1-1', '**\*.*'));
             filelist = filelist(~[filelist.isdir]);
         end
     else
         % Caso real
-        filelist = dir(fullfile('images/problem1.2', '**\*.*'));
+        filelist = dir(fullfile('images/problem1-2', '**\*.*'));
         filelist = filelist(~[filelist.isdir]);
+    end
+end
+
+
+function imPlate = detectPlateRegion(im, difficulty)
+    if(difficulty == 1)
+        % Controlled Environment (CE)
+        imPlate = detectPlateRegionCE(im);
+    else 
+        % Real Environment (RE)
+        imPlate = detectPlateRegionRE(im);
     end
 end
 
@@ -58,9 +64,9 @@ function imPlate = detectPlateRegionCE(im)
     v = imHSV(:,:,3);  % canal Value
     
     % Definir filtro de color (verde -> entorno controlado, blanco -> escenario)
-    hMin = 118/360; hMax = 183/360;
-    sMin =  80/255; sMax = 255/255;
-    vMin =  53/255; vMax = 175/255;
+    hMin = 91/360; hMax = 183/360;
+    sMin = 80/255; sMax = 255/255;
+    vMin = 53/255; vMax = 175/255;
 
     % Aplicar la mascara
     imMasked = (h>=hMin & h<=hMax) & (s>=sMin & s<=sMax) & (v>=vMin & v<=vMax);
@@ -81,6 +87,71 @@ function imPlate = detectPlateRegionCE(im)
     imPlate = imClean(rMin:rMax, cMin:cMax, :);
 end
 
+function imPlate = detectPlateRegionRE(im)
+    % 1) Conversión a gris y filtrado suave
+    imGray = rgb2gray(im);
+    imGray = medfilt2(imGray, [3 3]);
+    
+    % 2) Detección de bordes más precisa con Canny
+    edges = edge(imGray, 'Canny', [0.05 0.15]);
+    
+    %% 1. Aplicar operaciones morfologicas
+    imGray = rgb2gray(im);
+    imGray = medfilt2(imGray, [3 3]);
+    %figure('Name', 'Imagen de grises'); imshow(imGray);
+
+    % Apply morphological operations to enhance the detected edges
+    se = strel('disk', 1);
+    imDilated = imdilate(imGray, se);
+    imEroded = imerode(imGray, se);
+    imClean = imsubtract(imDilated, imEroded);
+    imClean = mat2gray(imClean);
+    figure('Name', 'Imagen limpia'); imshow(imClean);
+
+    imClean = imadjust(imClean, [0.2 .7], [0 1]);
+    imClean = bwpropfilt(logical(imClean), 'Area', 20); 
+    figure('Name', 'Imagen limpia'); imshow(imClean);
+    
+    %imClean = conv2(imClean, [1 1; 1 1], "same");
+    %imClean = imadjust(imClean, [0.7 1], [0 1]);
+    %imClean = bwpropfilt(logical(imClean), 'Area', 15); 
+    %figure('Name', 'Imagen limpia'); imshow(imClean);
+    
+    %% 2. Recortar la matricula
+    cc = bwconncomp(imClean);
+    Iprops = regionprops(cc, 'BoundingBox', 'Area');
+    
+    bb = Iprops(1).BoundingBox;
+    maxArea = 0;
+    for i=1:numel(Iprops)
+        boundingBox = Iprops(i).BoundingBox;
+        area = Iprops(i).Area;
+        aspectRatio = boundingBox(3) / boundingBox(4);
+        if maxArea < area && aspectRatio > 1.7 && aspectRatio < 2
+           maxArea = area;
+           bb = boundingBox;
+           figure('Name', num2str(area)); imshow(imcrop(imClean, bb));
+        end
+    end
+    
+    imPlate = imcrop(imClean, bb);
+    imPlate = bwareaopen(~imPlate, 500);
+    [h, ~] = size(imPlate);
+
+    Iprops=regionprops(imClean,'BoundingBox', 'Area', 'Image');
+    count = numel(Iprops);
+    %noPlate=[];
+    for i=1:count
+        ow = length(Iprops(i).Image(1,:));
+        oh = length(Iprops(i).Image(:,1));
+        if ow<(h/2) & oh>(h/3)
+            figure('Name', ['Letter' i]); imshow(Iprops(i).Image);
+        end
+    end
+
+    pause
+end
+
 function templates = loadTemplates(difficulty)
     % Carregar imatges de referència (0-9, A-Z) des de carpeta 'templates'
     symbols = ['0':'9' 'A':'Z'];
@@ -88,7 +159,7 @@ function templates = loadTemplates(difficulty)
     j = 1;
     for i = 1:maxSymbols
         character = symbols(i);
-        imgPath = fullfile(['templates/problem1.' num2str(difficulty)], [character '.png']);
+        imgPath = fullfile(['templates/problem1-' num2str(difficulty)], [character '.png']);
         if isfile(imgPath)
             templates.symbols{j} = character;
             templates.images{j}  = im2bw(imresize(imread(imgPath),[42 24]));
